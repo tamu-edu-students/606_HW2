a67e417b42028491256053950566f4238d5f975c
gjubey <jubey.s.garza@tamu.edu>
2024-02-21 21:54:49 -0600

    added sorting and new seeded values

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Seed the RottenPotatoes DB with some movies.
+more_movies = [
+  {:title => 'My Neighbor Totoro', :rating => 'G',
+    :release_date => '16-Apr-1988'},
+  {:title => 'Green Book', :rating => 'PG-13',
+    :release_date => '16-Nov-2018'},
+  {:title => 'Parasite', :rating => 'R',
+    :release_date => '30-May-2019'},
+  {:title => 'Nomadland', :rating => 'R',
+    :release_date => '19-Feb-2021'},
+  {:title => 'CODA', :rating => 'PG-13',
+    :release_date => '13-Aug-2021'},
+  {:title => 'Howls Moving Castle', :rating => 'PG-13',
+    :release_date => '1-Aug-2003'},
+  {:title => 'Spirited Away', :rating => 'PG-13',
+    :release_date => '13-Aug-2001'},
+  {:title => 'Whispers of the Heart', :rating => 'PG-13',
+    :release_date => '13-Aug-1987'}
+]
+
+more_movies.each do |movie|
+  Movie.create!(movie)
+end
